07114d2f318240c9ab73a67fedb3f11db5f28580
Colton Simmons <Colton@Coltons-MacBook-Pro.local>
2017-09-26 00:44:39 -0400

    comment

--- multimedia/MultimediaRootPaneContainer.java
+++
@@ -1,8 +0,0 @@
-package multimedia;
-import javax.swing.RootPaneContainer;
-
-public abstract interface MultimediaRootPaneContainer
-  extends RootPaneContainer
-{
-  public abstract String getParameter(String paramString);
-}
